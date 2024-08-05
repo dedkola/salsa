7159c53e6f52ff51a26112c4dafbf74adb38afcc
Kola <maiden.tech@gmail.com>
2024-08-05 20:37:23 +0300

    fix provider v.

--- terrraform/k3s.tf
+++ terrraform/k3s.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "registry.example.com/telmate/proxmox"
-      version = ">=1.0.0"
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc3"
     }
   }
   required_version = ">= 0.14"
