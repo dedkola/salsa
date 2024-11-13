94edfd4ee8aa8f9a2f972166a7420fb3d3621f17
Kola <maiden.tech@gmail.com>
2024-11-13 15:58:41 +0200

    new file:   terrraform/wsl.tf

---
+++ terrraform/wsl.tf
@@ -0,0 +1,24 @@
+provider "local" {
+}
+
+resource "null_resource" "export_wsl" {
+  provisioner "local-exec" {
+    command = "wsl --export Ubuntu-24.04 E:\\WSL\\ubuntu-backup.tar"
+  }
+}
+
+resource "null_resource" "import_wsl_1" {
+  depends_on = [null_resource.export_wsl]
+
+  provisioner "local-exec" {
+    command = "wsl --import U1 E:\\WSL\\U1 E:\\WSL\\ubuntu-backup.tar"
+  }
+}
+
+resource "null_resource" "import_wsl_2" {
+  depends_on = [null_resource.export_wsl]
+
+  provisioner "local-exec" {
+    command = "wsl --import U2 E:\\WSL\\U2 E:\\WSL\\ubuntu-backup.tar"
+  }
+}
